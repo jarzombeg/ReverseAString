83603ebf2e11c77aecb9b5dc3731da125a913d35
jarzombeg <jarzombeg@gmail.com>
2021-02-15 10:34:52 +0000

    First

---
+++ src/com/company/Program.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        char[] arr = text.toCharArray();
+        //your code goes here
+        int l = arr.length-1;
+        for (int i = l; i >= 0; i--) {
+            char a = arr[i];
+            System.out.print(a);
+        }
+    }
+}
+
